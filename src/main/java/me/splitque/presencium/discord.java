5be6deb43f42e7eafa814de2f73a45f09fed5a80
splittque <140718354+splittque@users.noreply.github.com>
2023-11-01 17:27:18 +0500

    Changed a code

--- src/main/java/me/splitque/presencium/discord.java
+++ src/main/java/me/splitque/presencium/discord.java
@@ -7,21 +7,25 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class discord {
-    public static final Logger logger = LogManager.getLogger();
     public static final DiscordRPC presencium = DiscordRPC.INSTANCE;
+    public static final Logger logger = LogManager.getLogger();
     public static final DiscordRichPresence presence = new DiscordRichPresence();
     public static final DiscordEventHandlers handlers = new DiscordEventHandlers();
 
-    public static void init() {
+    public static void start() {
         presencium.Discord_Initialize("1119950313047208006", handlers, true, null);
         presence.startTimestamp = System.currentTimeMillis() / 1000;
         presence.largeImageKey = "mclogo";
         presence.largeImageText = "Presencium by splittque";
         presencium.Discord_UpdatePresence(presence);
-        logger.info("[Presencium] RPC has been started!");
+        logger.info("[Presencium] Mod has been started!");
     }
-    public static void update(String details) {
-        presence.details = details;
+    public static void stop() {
+        presencium.Discord_Shutdown();
+        logger.info("[Presencium] Mod has been disabled!");
+    }
+    public static void update(String state) {
+        presence.details = state;
         presencium.Discord_UpdatePresence(presence);
     }
 }
--- src/main/java/me/splitque/presencium/states.java
+++ src/main/java/me/splitque/presencium/states.java
@@ -5,6 +5,7 @@ import net.minecraft.client.MinecraftClient;
 public class states {
     public static String single_state = "";
     public static String multi_state = "";
+    public static String realm_state = "";
     public static String main_state = "";
     public static String language = MinecraftClient.getInstance().getLanguageManager().getLanguage();
 
@@ -13,11 +14,13 @@ public class states {
             case "ru_ru":
                 single_state = "В одиночной игре";
                 multi_state = "В сетевой игре: ";
+                realm_state = "В Realms";
                 main_state = "В игре";
                 break;
             default:
                 single_state = "In singleplayer";
                 multi_state = "In multiplayer: ";
+                realm_state = "In Realms";
                 main_state = "In game";
                 break;
         }
