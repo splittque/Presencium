898440a10685c1fb7efe7bb55fc302f3db3aa954
splittque <140718354+splittque@users.noreply.github.com>
2023-12-06 00:57:44 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,19 +1,34 @@
 # Presencium
 ### Descriprion
-This mod show in discord your minecraft status.
+This mod show in discord your minecraft status
 
 ![Alt text](https://cdn-raw.modrinth.com/data/iNU1UQcw/images/f05b904ad665d718c6b5e25ce7372bcf99740b96.png)
 
 ### Supported languages
 - English
 - Русский (Russian)
-
-**If you want to support in translation, fill out this form: https://docs.google.com/forms/d/1D6zsJlmOnygzKOgrxNAYOJDfjp5qbaTvWG-ijuOn8Ds**
+- Français (French)
+- Deutsch (German)
+- Español (Spanish)
+- Português (Portuguese)
+- Italiano (Italian)
 
 ## Build
+### IDE
 1. Download a project
-2. Open terminal in the project
-3. Type gradlew init and gradlew genSources
+2. Open project in your IDE
+3. Change the code
+4. Build
+
+### non-IDE
+1. Download project
+2. Open terminal in project
+3. Type **gradlew init**
 4. Change the code
-5. Type gradlew build
-6. For testing, type in terminal gradlew downloadAssets, then type gradlew runClient
+5. For building certain project:
+   - fabric: **gradlew fabric:build**
+   - quilt: **gradlew quilt:build**
+6. For build all projects type in terminal **gradlew build**
+7. For testing, type in terminal **gradlew downloadAssets**, then:
+   - fabric: **gradlew fabric:runClient**
+   - quilt: **gradlew quilt:runClient**
---
+++ fabric/src/main/java/me/splitque/presencium/main.java
@@ -0,0 +1,47 @@
+package me.splitque.presencium;
+
+import me.splitque.presencium.config.config;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.text.Text;
+
+public class main implements ModInitializer {
+    Text state = Text.of("");
+
+    @Override
+    public void onInitialize() {
+        ClientLifecycleEvents.CLIENT_STARTED.register(client -> {
+            discord.start();
+            config.load();
+        });
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client.isInSingleplayer()) {
+                state = Text.translatable("single_state");
+                discord.update(state.getString());
+            } else if (client.getCurrentServerEntry() != null) {
+                if (client.getCurrentServerEntry().isRealm()) {
+                    state = Text.translatable("realm_state");
+                    discord.update(state.getString());
+                }
+                if (config.get("server_ip")) {
+                    String address = client.getCurrentServerEntry().address;
+                    state = Text.translatable("multi_state");
+                    discord.update(state.getString() + ": " + address);
+                } else if (!config.get("server_ip")) {
+                    state = Text.translatable("multi_state");
+                    discord.update(state.getString());
+                }
+            } else {
+                state = Text.translatable("main_state");
+                discord.update(state.getString());
+            }
+        });
+
+        ClientLifecycleEvents.CLIENT_STOPPING.register(client -> {
+            discord.stop();
+            config.save();
+        });
+    }
+}
