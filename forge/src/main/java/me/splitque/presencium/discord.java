9df1e31394f3e6c2ef71cc1faf0c0801eaae5b86
splittque <140718354+splittque@users.noreply.github.com>
2024-02-12 15:24:22 +0500

    Add files via upload

--- forge/src/main/java/me/splitque/presencium/discord.java
+++ forge/src/main/java/me/splitque/presencium/discord.java
@@ -8,14 +8,17 @@ public class discord {
     public static final DiscordRPC presencium = DiscordRPC.INSTANCE;
     public static final Logger logger = LogManager.getLogger();
     public static final DiscordRichPresence presence = new DiscordRichPresence();
+    public static final DiscordEventHandlers handlers = new DiscordEventHandlers();
 
     public static void start() {
-        presencium.Discord_Initialize("1119950313047208006", null, true, null);
+        presencium.Discord_Initialize("1119950313047208006", handlers, true, null);
         presence.startTimestamp = System.currentTimeMillis() / 1000;
         presence.largeImageKey = "mclogo";
         presence.largeImageText = "Presencium by splittque";
         presencium.Discord_UpdatePresence(presence);
-        logger.info("[Presencium] Mod has been started!");
+
+        handlers.disconnected = (errorCode, message) -> start();
+        handlers.ready = (user) -> logger.info("[Presencium] DiscordRPC has been started!");
     }
     public static void stop() {
         presencium.Discord_Shutdown();
