f09e41454199678886a2efb6d0a2c25e80c88236
splittque <shestqkov@gmail.com>
2024-08-30 02:44:50 +0500

    fabric api returned back :)

--- src/main/java/me/splitque/presencium/main.java
+++ src/main/java/me/splitque/presencium/main.java
@@ -2,53 +2,43 @@ package me.splitque.presencium;
 
 import me.splitque.presencium.config.config;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.client.MinecraftClient;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.text.Text;
 
 public class main implements ModInitializer {
-    MinecraftClient mc = MinecraftClient.getInstance();
     Text state = Text.of("");
 
     @Override
     public void onInitialize() {
-        discord.start();
-        config.load();
+        ClientLifecycleEvents.CLIENT_STARTED.register(client -> {
+            discord.start();
+            config.load();
+        });
 
-        Runnable rpc_updater = new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    if (mc.isInSingleplayer()) {
-                        state = Text.translatable("single_state");
-                        discord.update(state.getString());
-                    } else if (mc.getCurrentServerEntry() != null) {
-                        if (mc.getCurrentServerEntry().isRealm()) {
-                            state = Text.translatable("realm_state");
-                            discord.update(state.getString());
-                        }
-
-                        if (config.get("server_ip")) {
-                            String address = mc.getCurrentServerEntry().address;
-                            state = Text.translatable("multi_state");
-                            discord.update(state.getString() + ": " + address);
-                        } else if (!config.get("server_ip")) {
-                            state = Text.translatable("multi_state");
-                            discord.update(state.getString());
-                        }
-                    } else {
-                        state = Text.translatable("main_state");
-                        discord.update(state.getString());
-                    }
-
-                    try {
-                        Thread.sleep(16);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client.isInSingleplayer()) {
+                state = Text.translatable("single_state");
+            } else if (client.getCurrentServerEntry() != null) {
+                if (client.getCurrentServerEntry().isRealm()) {
+                    state = Text.translatable("realm_state");
                 }
+
+                if (config.get("server_ip")) {
+                    String address = client.getCurrentServerEntry().address;
+                    state = Text.translatable("multi_state" + ": " + address);
+                } else if (!config.get("server_ip")) {
+                    state = Text.translatable("multi_state");
+                }
+            } else {
+                state = Text.translatable("main_state");
             }
-        };
-        Thread thread = new Thread(rpc_updater);
-        thread.start();
+            discord.update(state.getString());
+        });
+
+        ClientLifecycleEvents.CLIENT_STOPPING.register(client -> {
+            discord.stop();
+            config.save();
+        });
     }
 }
