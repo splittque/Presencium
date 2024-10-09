54cb7505fdc0b3d8f878b031701bada363cf3453
splittque <shestqkov@gmail.com>
2024-10-09 21:57:48 +0500

    class for log messages

---
+++ common/src/main/java/me/splitque/common/Logging.java
@@ -0,0 +1,19 @@
+package me.splitque.common;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Logging {
+    private static Logger logger = LogManager.getLogger("Presencium");
+
+    public static void start() {
+        logger.info("DiscordRPC has been started!");
+    }
+    public static void end(boolean isError, String error) {
+        if (isError) {
+            logger.info("DiscordRPC has been stopped! Error code: " + error);
+        } else if (!isError) {
+            logger.info("DiscordRPC has been stopped!");
+        }
+    }
+}
