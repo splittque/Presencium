992394730f5930c6ec969cd0f77b8c83b9d87517
splittque <shestqkov@gmail.com>
2024-10-10 10:09:12 +0500

    upgrade discord rpc handler (work in progress)

--- common/src/main/java/me/splitque/common/DiscordRPCHandler.java
+++ common/src/main/java/me/splitque/common/DiscordRPCHandler.java
@@ -1,54 +1,80 @@
 package me.splitque.common;
 
 import club.minnced.discord.rpc.*;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.util.Calendar;
 
 public class DiscordRPCHandler {
-    private static DiscordRPC presencium = DiscordRPC.INSTANCE;
-    private static Boolean isWorking;
-    private static Logger log = LogManager.getLogger("Presencium");
-    private static DiscordRichPresence presence = new DiscordRichPresence();
+    private static Boolean rpcIsStarted;
+    private static DiscordRPC discordRPC = DiscordRPC.INSTANCE;
     private static DiscordEventHandlers handlers = new DiscordEventHandlers();
+    private static DiscordRichPresence presence = new DiscordRichPresence();
     private static Calendar calendar = Calendar.getInstance();
 
     public static void start() {
-        presencium.Discord_Initialize("1119950313047208006", handlers, true, null);
+        handlers.ready = (user) -> {
+            Logging.start();
+            rpcIsStarted = true;
+        };
+        handlers.disconnected = (errorCode, message) -> {
+            Logging.end(false, null);
+            rpcIsStarted = false;
+        };
+
+        Runnable callbacks = new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    Logging.debug();
+                    discordRPC.Discord_RunCallbacks();
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        Thread thread = new Thread(callbacks);
+        thread.setName("RPC-Callback-Handler");
+        if () {
+            thread.start();
+        }
+
+        discordRPC.Discord_Initialize("1119950313047208006", handlers, true, null);
+        discordRPC.Discord_UpdateHandlers(handlers);
         presence.startTimestamp = System.currentTimeMillis() / 1000;
         presence.largeImageKey = "mclogo";
-        if (calendar.get(Calendar.DAY_OF_MONTH) == 26 && calendar.get(Calendar.MONTH) == Calendar.OCTOBER) {
-            presence.largeImageText = "HAPPY BIRTHDAY, PRESENCIUM!";
-        } else {
-            presence.largeImageText = "Presencium by splittque";
-        }
-        presencium.Discord_UpdatePresence(presence);
-
-        handlers.errored = (errorCode, message) -> start();
-        handlers.disconnected = (errorCode, message) -> start();
-        handlers.ready = (user) -> log.info("DiscordRPC has been started!");
-        isWorking = true;
+        presence.largeImageText = getImageText();
+        discordRPC.Discord_UpdatePresence(presence);
     }
     public static void stop() {
-        presencium.Discord_Shutdown();
-        log.info("DiscordRPC has been stopped!");
-        isWorking = false;
+        discordRPC.Discord_Shutdown();
     }
     public static void update(String state) {
-        if (isWorking) {
-            if (SettingsHandler.get("rpc_onoff")) {
-                presence.details = state;
-                presencium.Discord_UpdatePresence(presence);
+        if (rpcIsStarted != null) {
+            if (rpcIsStarted) {
+                if (SettingsHandler.get("rpc_switcher")) {
+                    presence.details = state;
+                    discordRPC.Discord_UpdatePresence(presence);
+                }
+                if (!SettingsHandler.get("rpc_switcher")) {
+                    stop();
+                }
             }
-            if (!SettingsHandler.get("rpc_onoff")) {
-                stop();
+            if (!rpcIsStarted) {
+                if (SettingsHandler.get("rpc_switcher")) {
+                    start();
+                }
             }
         }
-        if (!isWorking) {
-            if (SettingsHandler.get("rpc_onoff")) {
-                start();
-            }
+    }
+
+    private static String getImageText() {
+        if (calendar.get(Calendar.DAY_OF_MONTH) == 26 && calendar.get(Calendar.MONTH) == Calendar.OCTOBER) {
+            return "HAPPY BIRTHDAY, PRESENCIUM!";
+        } else {
+            return "Presencium by splittque";
         }
     }
 }
