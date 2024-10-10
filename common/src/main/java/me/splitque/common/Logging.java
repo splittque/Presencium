a810587a04de0a9b148dd23d5deb5f88b5a62de9
splittque <shestqkov@gmail.com>
2024-10-10 10:09:54 +0500

    debug message

--- common/src/main/java/me/splitque/common/Logging.java
+++ common/src/main/java/me/splitque/common/Logging.java
@@ -9,6 +9,9 @@ public class Logging {
     public static void start() {
         logger.info("DiscordRPC has been started!");
     }
+    public static void debug() {
+        logger.info("DEBUG");
+    }
     public static void end(boolean isError, String error) {
         if (isError) {
             logger.info("DiscordRPC has been stopped! Error code: " + error);
