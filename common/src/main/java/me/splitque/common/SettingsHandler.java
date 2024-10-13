9a51863b0a3bc3c10e85576e89c48b1cec8c1508
splittque <shestqkov@gmail.com>
2024-10-13 10:48:08 +0500

    renamed variables

--- common/src/main/java/me/splitque/common/SettingsHandler.java
+++ common/src/main/java/me/splitque/common/SettingsHandler.java
@@ -5,15 +5,17 @@ import me.splitque.configuration.ConfigHandler;
 public class SettingsHandler {
     public static ConfigHandler configHandler;
 
-    public static void load(String path) {
+    public static void loadSettings(String path) {
         configHandler = new ConfigHandler(false, path, "presencium", "PROPERTIES");
         configHandler.putOption("rpc_switcher", "true");
         configHandler.putOption("show_server_ip", "true");
+        configHandler.putOption("debug", "false");
     }
-    public static void set(String name, Boolean value) {
+    public static void setOption(String name, Boolean value) {
         configHandler.setOption(name, String.valueOf(value));
+        Logging.settingChanged(name, String.valueOf(value));
     }
-    public static Boolean get(String name) {
+    public static Boolean getOption(String name) {
         return configHandler.getBoolean(name);
     }
 }
