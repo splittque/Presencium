6bd30c0225583717c6454455df0e92e18819dc12
splittque <shestqkov@gmail.com>
2024-10-13 10:46:50 +0500

    end of work on improvement discord rpc handler

--- common/src/main/java/me/splitque/common/DiscordRPCHandler.java
+++ common/src/main/java/me/splitque/common/DiscordRPCHandler.java
@@ -7,66 +7,72 @@ import java.util.Calendar;
 public class DiscordRPCHandler {
     private static Boolean rpcIsStarted;
     private static DiscordRPC discordRPC = DiscordRPC.INSTANCE;
-    private static DiscordEventHandlers handlers = new DiscordEventHandlers();
     private static DiscordRichPresence presence = new DiscordRichPresence();
+    private static DiscordEventHandlers handlers = new DiscordEventHandlers();
     private static Calendar calendar = Calendar.getInstance();
 
-    public static void start() {
-        handlers.ready = (user) -> {
-            Logging.start();
-            rpcIsStarted = true;
-        };
-        handlers.disconnected = (errorCode, message) -> {
-            Logging.end(false, null);
-            rpcIsStarted = false;
-        };
-
+    public static void startCallbacks() {
         Runnable callbacks = new Runnable() {
             @Override
             public void run() {
                 while (true) {
-                    Logging.debug();
                     discordRPC.Discord_RunCallbacks();
+                    Logging.debug("callback", 2);
                     try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {}
                 }
             }
         };
         Thread thread = new Thread(callbacks);
-        thread.setName("RPC-Callback-Handler");
-        if () {
-            thread.start();
-        }
+        thread.setName("Presencium-RPC-Callbacks");
+        thread.start();
+    }
 
+    public static void startRPC() {
+        handlers.ready = (user) -> {
+            Logging.started();
+            rpcIsStarted = true;
+        };
+        handlers.disconnected = (errorCode, message) -> {
+            Logging.stopped();
+            rpcIsStarted = false;
+        };
         discordRPC.Discord_Initialize("1119950313047208006", handlers, true, null);
-        discordRPC.Discord_UpdateHandlers(handlers);
         presence.startTimestamp = System.currentTimeMillis() / 1000;
         presence.largeImageKey = "mclogo";
         presence.largeImageText = getImageText();
         discordRPC.Discord_UpdatePresence(presence);
+        Logging.debug("rpc init", 1);
     }
-    public static void stop() {
+    public static void stopRPC() {
         discordRPC.Discord_Shutdown();
+        Logging.stopped();
+        rpcIsStarted = false;
     }
-    public static void update(String state) {
+    public static void updateState(String translatedState, String vanillaState) {
         if (rpcIsStarted != null) {
             if (rpcIsStarted) {
-                if (SettingsHandler.get("rpc_switcher")) {
-                    presence.details = state;
+                if (SettingsHandler.getOption("rpc_switcher")) {
+                    Logging.debug("true", 3);
+                    presence.details = translatedState;
+                    Logging.stateUpdate(vanillaState);
                     discordRPC.Discord_UpdatePresence(presence);
                 }
-                if (!SettingsHandler.get("rpc_switcher")) {
-                    stop();
+                if (!SettingsHandler.getOption("rpc_switcher")) {
+                    Logging.debug("false", 3);
+                    stopRPC();
                 }
             }
             if (!rpcIsStarted) {
-                if (SettingsHandler.get("rpc_switcher")) {
-                    start();
+                if (SettingsHandler.getOption("rpc_switcher")) {
+                    Logging.debug("true", 4);
+                    startRPC();
                 }
             }
+        } else {
+            Logging.debug("false", 5);
+            rpcIsStarted = false;
         }
     }
 
