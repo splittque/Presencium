8f27fdd7357c8ae41349b7584be2538a72d636ad
splittque <shestqkov@gmail.com>
2024-10-13 10:47:24 +0500

    new messages

--- common/src/main/java/me/splitque/common/Logging.java
+++ common/src/main/java/me/splitque/common/Logging.java
@@ -5,18 +5,26 @@ import org.apache.logging.log4j.Logger;
 
 public class Logging {
     private static Logger logger = LogManager.getLogger("Presencium");
+    private static String _state = "";
 
-    public static void start() {
+    public static void started() {
         logger.info("DiscordRPC has been started!");
     }
-    public static void debug() {
-        logger.info("DEBUG");
+    public static void stopped() {
+        logger.info("DiscordRPC has been stopped!");
     }
-    public static void end(boolean isError, String error) {
-        if (isError) {
-            logger.info("DiscordRPC has been stopped! Error code: " + error);
-        } else if (!isError) {
-            logger.info("DiscordRPC has been stopped!");
+    public static void debug(String message, int code) {
+        if (SettingsHandler.getOption("debug")) {
+            logger.info("[DEBUG]: " + code + " - " + message);
         }
+    }
+    public static void stateUpdate(String state) {
+        if (!_state.equals(state)) {
+            _state = state;
+            logger.info("State changed to: " + state);
+        }
+    }
+    public static void settingChanged(String name, String value) {
+        logger.info("Setting " + name + " changed to: " + value);
     }
 }
