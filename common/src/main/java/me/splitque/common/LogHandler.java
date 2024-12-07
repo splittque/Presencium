ecc18859375a00fd64f3d639a585a59e421b6773
splittque <140718354+splittque@users.noreply.github.com>
2024-12-07 23:43:52 +0500

    1.21.4 and deleted log library

--- common/src/main/java/me/splitque/common/LogHandler.java
+++ common/src/main/java/me/splitque/common/LogHandler.java
@@ -1,28 +1,29 @@
 package me.splitque.common;
 
-import me.splitque.log.Logger;
-import me.splitque.log.variables.Color;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class LogHandler {
-    private static Logger log = new Logger("Presencium", Color.GREEN, false, true);
+    private static Logger log = LogManager.getLogger("Presencium");
     private static String _state = "";
 
     public static void started() {
-        log.info("DiscordRPC has been started!", null);
+        log.info("DiscordRPC has been started!");
     }
     public static void stopped() {
-        log.info("DiscordRPC has been stopped!", null);
+        log.info("DiscordRPC has been stopped!");
     }
     public static void debug(String message, int code) {
-        log.custom(message, null, code, "DEBUG", null,  SettingsHandler.getOption("debug"));
+        log.warn("[DEBUG] " + code + ": " + message); // [DEBUG] 1: message
     }
     public static void stateUpdate(String state) {
         if (!_state.equals(state)) {
             _state = state;
-            log.info("State changed to: " + state, null);
+            log.info("State changed to: " + state);
         }
     }
     public static void settingChanged(String key, String value) {
-        log.info("Setting " + key + " changed to: " + value, null);
+        log.info("Setting " + key + " changed to: " + value);
     }
 }
